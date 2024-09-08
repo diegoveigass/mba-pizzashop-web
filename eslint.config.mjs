28159aea2e6a48f01fbd77b959ecf9329b6052b5
Diego Veiga <sdiegoveiga@gmail.com>
2024-09-08 14:38:48 -0300

    Configuring eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import config from '@rocketseat/eslint-config/react.mjs'
+import eslintSimpleImportPlugin from 'eslint-plugin-simple-import-sort'
 
-export default config
+export default [
+  ...config,
+  {
+    plugins: {
+      'simple-import-sort': eslintSimpleImportPlugin,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+    },
+  },
+]
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import '@/styles/global.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App'
 
-import '@/styles/global.css'
+import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
