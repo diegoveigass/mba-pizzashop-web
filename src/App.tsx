46db8b39fecb46ec15f1db16d246c1c39f8b712e
Diego Veiga <sdiegoveiga@gmail.com>
2024-09-09 08:09:52 -0300

    Toaster sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster richColors />
       <Helmet titleTemplate="%s | pizzashop" />
       <RouterProvider router={router} />
     </HelmetProvider>
