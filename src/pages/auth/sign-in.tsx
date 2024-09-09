785c49d732e3884a3fccc8be3cf16b6f08b04d09
Diego Veiga <sdiegoveiga@gmail.com>
2024-09-09 07:45:34 -0300

    Configuring react-hook-form in sign-in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,28 +1,65 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInFormSchema = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInFormSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>({
+    resolver: zodResolver(signInFormSchema),
+  })
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    console.log(data)
+  }
+
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
             <p className="text-sm text-muted-foreground">
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
 
-          <form className="w-full space-y-4">
+          <form
+            className="w-full space-y-4"
+            onSubmit={handleSubmit(handleSignIn)}
+          >
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" placeholder="john.doe@example.com" />
+              <Input
+                id="email"
+                placeholder="john.doe@example.com"
+                {...register('email')}
+              />
             </div>
-            <Button type="submit" className="w-full">Acessar painel</Button>
+            <Button
+              type="submit"
+              className="w-full"
+              disabled={isSubmitting}
+            >
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
