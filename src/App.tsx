69b838b4175775d3773aa7c7d72a824506d479c8
Diego Veiga <sdiegoveiga@gmail.com>
2024-09-13 19:32:16 -0300

    Configuring API and react-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
-
         <Toaster richColors />
         <Helmet titleTemplate="%s | pizzashop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
-
     </HelmetProvider>
   )
 }
