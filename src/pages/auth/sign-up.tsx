23a6afa1ae04189a1fd88845f411033ae9cc8228
Diego Veiga <sdiegoveiga@gmail.com>
2024-09-16 07:43:12 -0300

    Register restaurant function

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -8,6 +8,8 @@ import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useMutation } from '@tanstack/react-query'
+import { registerRestaurant } from '@/api/register-restaurant'
 
 const signUpFormSchema = z.object({
   restaurantName: z.string(),
@@ -29,15 +31,25 @@ export function SignUp() {
     resolver: zodResolver(signUpFormSchema),
   })
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignUp(data: SignUpForm) {
+    const { email, managerName, phone, restaurantName } = data
+
     try {
-      await new Promise(resolve => setTimeout(resolve, 2000))
-      console.log(data)
+      await registerRestaurantFn({
+        email,
+        managerName,
+        phone,
+        restaurantName,
+      })
 
       toast.success('Restaurante cadastrado com sucesso.', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+          onClick: () => navigate(`/sign-in?email=${email}`),
         },
       })
     } catch {
@@ -50,9 +62,7 @@ export function SignUp() {
       <Helmet title="Cadastro" />
       <div className="p-8">
         <Button variant="ghost" asChild className="absolute right-8 top-8">
-          <Link to="/sign-in">
-            Fazer login
-          </Link>
+          <Link to="/sign-in">Fazer login</Link>
         </Button>
 
         <div className="w-[350px] flex flex-col justify-center gap-6">
@@ -71,18 +81,12 @@ export function SignUp() {
           >
             <div className="space-y-2">
               <Label htmlFor="restaurantName">Nome do estabelecimento</Label>
-              <Input
-                id="restaurantName"
-                {...register('restaurantName')}
-              />
+              <Input id="restaurantName" {...register('restaurantName')} />
             </div>
 
             <div className="space-y-2">
               <Label htmlFor="managerName">Seu nome</Label>
-              <Input
-                id="managerName"
-                {...register('managerName')}
-              />
+              <Input id="managerName" {...register('managerName')} />
             </div>
 
             <div className="space-y-2">
@@ -97,24 +101,22 @@ export function SignUp() {
 
             <div className="space-y-2">
               <Label htmlFor="phone">Seu celular</Label>
-              <Input
-                id="phone"
-                type="tel"
-                {...register('phone')}
-              />
+              <Input id="phone" type="tel" {...register('phone')} />
             </div>
-            <Button
-              type="submit"
-              className="w-full"
-              disabled={isSubmitting}
-            >
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
               Finalizar cadastro
             </Button>
 
             <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
               Ao continuar, você concorda com nossos{' '}
-              <a href="#" className="underline underline-offset-4">Termos de serviço</a>{' '}
-              e{' '} <a href="#" className="underline underline-offset-4">políticas de privacidade</a>.
+              <a href="/" className="underline underline-offset-4">
+                Termos de serviço
+              </a>{' '}
+              e{' '}
+              <a href="/" className="underline underline-offset-4">
+                políticas de privacidade
+              </a>
+              .
             </p>
           </form>
         </div>
