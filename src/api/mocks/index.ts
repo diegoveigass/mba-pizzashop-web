f8e067d7af77cae0d130a4aadd07c10e145294ca
Diego Veiga <diego.veiga@xmobots.com.br>
2024-09-24 19:42:10 -0300

    Create mock api call's to sign-in and create restaurant

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,7 +1,9 @@
 import { env } from '@/env'
 import { setupWorker } from 'msw/browser'
+import { registerRestaurantMock } from './register-restaurant-mock'
+import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker()
+export const worker = setupWorker(signInMock, registerRestaurantMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
